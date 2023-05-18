479fb4779592aea8874b1d480f094ada346d4c69
Renato Moraes <rnmoraes@inoa.com.br>
2023-05-18 18:09:49 -0300

    wip

--- deploy/common/functions.sh
+++ deploy/common/functions.sh
@@ -14,7 +14,7 @@ source ${script_parent}/common/vars.sh
 #####################################
 # SCRIPT AUXILIARY FUNCTIONS
 #####################################
-orange_title_message(){
+orange_title_message() {
   local message=$1
   printf "\n"
   print_timestamp "${ORANGE}###########################################${NC}"
@@ -22,7 +22,7 @@ orange_title_message(){
   print_timestamp "${ORANGE}###########################################\n${NC}"
 }
 
-cyan_secondary_message(){
+cyan_secondary_message() {
   local message=$1
   printf "\n"
   print_timestamp "${CYAN}********************************************${NC}"
@@ -30,7 +30,7 @@ cyan_secondary_message(){
   print_timestamp "${CYAN}********************************************\n${NC}"
 }
 
-green_secondary_message(){
+green_secondary_message() {
   local message=$1
   printf "\n"
   print_timestamp "${GREEN}********************************************${NC}"
@@ -102,15 +102,15 @@ request_user() {
   echo ${!user1}
 }
 
-ask_user () {
-    message=$1
-    read -p "$message " yn
-    echo >&2
-    case $yn in
-        [Yy]* ) ;;
-        [Nn]* ) exit;;
-        * ) print_timestamp "Please answer yes or no.";;
-    esac
+ask_user() {
+  message=$1
+  read -p "$message " yn
+  echo >&2
+  case $yn in
+  [Yy]*) ;;
+  [Nn]*) exit ;;
+  *) print_timestamp "Please answer yes or no." ;;
+  esac
 }
 
 #####################################
@@ -119,20 +119,23 @@ ask_user () {
 
 migrate_routine() {
   ${script_parent}/run.sh -c up -r migrate
-  local at_migrate_container_name=`docker ps -f name="alphatools_migrate" --format='{{ .Names}}' | awk 'NR==1'`
-  if `is_container_running ${at_migrate_container_name}`; then
+  local at_migrate_container_name=$(docker ps -f name="alphatools_migrate" --format='{{ .Names}}' | awk 'NR==1')
+  if $(is_container_running ${at_migrate_container_name}); then
     local logfile_name="$(date +'%Y%m%d%H%M%Z')migrate.log"
     print_timestamp "Migrate logs are available at ${root_dir}/logs/migrate/${logfile_name}"
-    docker ps -a | awk '/alphatools_migrate/ {print "docker logs -f "$1}' | bash -x &> "${root_dir}/logs/migrate/${logfile_name}" &
+    docker ps -a | awk '/alphatools_migrate/ {print "docker logs -f "$1}' | bash -x &>"${root_dir}/logs/migrate/${logfile_name}" &
     print_timestamp "AlphaTools Migrate is running... "
-    until [ "`docker ps -a | awk '/alphatools_migrate/ {print \"docker inspect "$1" --format='\''{{ .State.Running}}'\''\"}' | bash -x 2>/dev/null`" = "false" > /dev/null 2>&1 ]; do sleep 1; printf "."; done
+    until [ "$(docker ps -a | awk '/alphatools_migrate/ {print \"docker inspect "$1" --format='\''{{ .State.Running}}'\''\"}' | bash -x 2>/dev/null)" = "false" ] >/dev/null 2>&1; do
+      sleep 1
+      printf "."
+    done
     ${script_parent}/run.sh -c down -r migrate
   else
     exit 1
   fi
 }
 
-get_secrets(){
+get_secrets() {
   local env_file_suffix=$1
   cyan_secondary_message "Getting environment variables from $env_file_suffix.env file"
   if [ ! -f ${root_dir}/$env_file_suffix.env ]; then
@@ -142,20 +145,34 @@ get_secrets(){
 }
 
 install_docker() {
-  if [ ! "`whoami`" = "root" ];then
+  if [ ! "$(whoami)" = "root" ]; then
     print_timestamp "${RED}You must be root to install Docker, run 'sudo -s' and try again${NC}"
     exit 1
   fi
 
-  if [[ $(cat /proc/version) =~ (ubuntu|debian) ]]; then
+  if [[ $(cat /proc/version) =~ "ubuntu" ]]; then
     apt-get update
     apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
     apt-get update
     apt-get install -y docker-ce docker-ce-cli containerd.io
     apt-get install -y docker-compose-plugin
+  fi
+
+  if [[ $(cat /proc/version) =~ "debian" ]]; then
+    apt-get update
+    install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    chmod a+r /etc/apt/keyrings/docker.gpg
+    echo \
+    "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    apt-get update
+    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
   fi
 
   if [[ $(cat /proc/version) =~ "centos" ]]; then
@@ -167,13 +184,13 @@ install_docker() {
   fi
   systemctl enable docker
 
-#  curl -SL "https://github.com/docker/compose/releases/download/v${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose --create-dirs
-#  chmod +x ~/.docker/cli-plugins/docker-compose
+  #  curl -SL "https://github.com/docker/compose/releases/download/v${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o ~/.docker/cli-plugins/docker-compose --create-dirs
+  #  chmod +x ~/.docker/cli-plugins/docker-compose
 }
 
 is_container_running() {
   local container_name=$1
-  if [ "`docker container inspect -f '{{.State.Running}}' $container_name`" = 'true' ]; then
+  if [ "$(docker container inspect -f '{{.State.Running}}' $container_name)" = 'true' ]; then
     return 0
   else
     return 1
