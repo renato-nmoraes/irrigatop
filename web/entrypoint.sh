c8d62acd9f8d62956081fcaa282b7a86f6682012
Renato Moraes <rnmoraes@inoa.com.br>
2023-05-19 01:15:00 -0300

    wip

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -3,11 +3,12 @@
 # Initialize the database (if not already initialized)
 flask db init
 
+# Run database migrations (if applicable)
+flask db migrate
+
 # Apply any pending database migrations
 flask db upgrade
 
-# Run database migrations (if applicable)
-flask db migrate
 
 # Start the application with Gunicorn
 exec gunicorn --bind 0.0.0.0:5000 app:app
