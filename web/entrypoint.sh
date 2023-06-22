23081c575e315df39a09e4eadaed5d8813258950
Renato Moraes <renato.moraes@botafogotech.com.br>
2023-06-22 00:32:58 -0300

    Revert "feat: reporting from mqtt"
    This reverts commit afbbb95488126fada65fdf2900035a956f898868.

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -11,8 +11,4 @@ flask db upgrade
 
 
 # Start the application with Gunicorn
-if [[ $FLASK_DEBUG == "1" ]]; then
-    python -m debugpy --listen 0.0.0.0:5678 --wait-for-client -m gunicorn --bind 0.0.0.0:5000 app:app
-else
-    exec gunicorn --bind 0.0.0.0:5000 app:app
-fi
+exec gunicorn --bind 0.0.0.0:5000 app:app
